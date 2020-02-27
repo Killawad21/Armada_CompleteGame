ce78a2ef88b3fabf4a4e0f385bf72c792fc7d879
Logan <02logan.pennock@gulllakecs.org>
2020-02-27 09:58:18 -0500

    Made switching between card possible

--- app/src/main/java/com/example/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/myapplication/Main3Activity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+    public void moveToActivityTwo(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
